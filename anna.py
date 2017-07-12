7a73a704cb7f6cabb344c1aa7cd4126160f9c49b
Jian Liang <lj299792458@gmail.com>
2017-07-12 15:24:26 -0400

    use the data of 5000 conf to have better distribution

--- anna.py
+++ anna.py
@@ -1,10 +1,31 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-data = np.load('two_p2_0_bin.npy')
-print(data.shape)
+ip = 0
+io = 0
+it = 0
+
+data = np.load('two_p2_%d.npy' % ip)
+nc = data.shape[0]
+no = data.shape[1]
+nt = data.shape[2]
 data_ave = np.average(data, 0)
-data_err = np.std(data, 0)/np.sqrt(data.shape[0] - 1)
-print(data_ave[0, :16].real)
-print(data_ave[0, :16].imag)
-print(data_err[0, :16])
+data_err = np.std(data, 0)/np.sqrt(nc - 1)
 
+data = data[:, io, it]
+
+data_imag = data[...].imag
+data_imag = np.sort(data_imag)
+st = data_imag[0]
+ed = data_imag[-1]
+n_sample0 = 100
+n_sample = n_sample0 + 1
+interval = (ed - st) / n_sample0
+distribution = np.zeros(n_sample)
+for ele in data_imag:
+    i = int((ele-st)//interval)
+    distribution[i] += 1
+plt.errorbar(np.arange(st, ed + interval, interval), distribution, yerr=np.sqrt(distribution), fmt='x')
+plt.grid()
+plt.show()
+plt.close()
